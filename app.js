78ddd082212e1b128229d439ea145ecbe1b860c3
dwlayton <dwlayton@comcast.net>
2015-06-01 14:08:41 -0700

    begin commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 // The number of milliseconds in one day
 //set to 86400000 ms for normal operations
 
-var oneDay = 0;
+var oneDay = 1000;
 
 // Use compress middleware to gzip content
 
@@ -14,8 +14,8 @@ app.use(compress());
 // Serve up content from public directory
 app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
 
-app.set('view engine', 'ejs');
 
+app.set('view engine', 'ejs');
 
 
 var http = require('http').Server(app);
@@ -36,63 +36,91 @@ http.listen(3000, function(){
 });
 
 app.use('/',function (req, res, next) {
+
     console.log('Time:', Date.now());
 
     io.on("connection", function (socket) {
 
-        request('http://freegeoip.net/json/', function (error, response, body) {
+        request('http://api.wunderground.com/api/9d0056d1d372fb05/conditions/q/autoip.json', function (error, response, body) {
             if (!error && response.statusCode == 200) {
 
-                var geoip = JSON.parse(body); // parse weather data
+                var conditions = JSON.parse(body); // parse weather data
 
-                var geodat ={city:geoip.city, region: geoip.region_code, lat:geoip.latitude, long: geoip.longitude};
+                var geodat ={city: conditions.current_observation.display_location.full};
 
                 io.emit("geoout", geodat);
 
-                var lat = geoip.latitude;
-                var long = geoip.longitude;
+                var lat = conditions.current_observation.observation_location.latitude;
+                var long = conditions.current_observation.observation_location.longitude;
+
+
+
+                var To = conditions.current_observation.temp_c; // outdoor air temperature, deg C
+                var RHout = conditions.current_observation.relative_humidity; // relative humidity of outdoor air, %
+                console.log(RHout);
+
+                var Td = conditions.current_observation.dewpoint_c; // dewpoint of outdoor air, degC
+
+                var RHo = 100*(Math.exp((17.625*Td)/(243.04+Td))/Math.exp((17.625*To)/(243.04+To))); //calculate RH from dewpoint, Td degC
+
+                console.log(RHo);
+
+                var S = 250; // emission rate of water vapor into indoor air from various sources, gm/hour
+                var A = 158; // floor area of house, m^2
+                var Hc = 2.8; // average ceiling height, m
+                var V = A * Hc; // house volume, m^3
+                var AER = 0.3; // air exchange rate, exchanges/hr
+                var Alpha = 0.65; // rate of water vapor uptake into building materials, fraction/hr
+                var Beta = 0.17; // rate of water vapor release from building materials, fraction/hr
+                var Tin = 26; // indoor temperature, deg C
+                var Wsat = (100000 * (6.112 * Math.exp(17.62 * Tin / (243.12 + Tin))) / ((273.15 + Tin) * 461.5)); // saturated water vapor concentration at indoor temperature, gm/m^3
+
+                console.log(Wsat);
+
+                var Wout = 216.7*(RHo/100)*6.112*Math.exp(17.62 * To / (243.12 + To))/(243.12+To); //Calculates absolute humidity of outdoor air, gm/m^3
+
+                console.log(Wout);
+
+                var Win = (S / V + AER * Wout + Beta * Wsat) / (AER + Alpha); // calculates water vapor concentration in indoor air, gm/m^3
+                console.log(Win);
+                var RHin = (Win / Wsat) * 100; // relative humidity of indoor air, %
+
+
+                console.log(RHin);
+
+                var type = 0;
+                if (RHin <= 30) {
+                    type = 4;
+                }
+                else if (RHin <= 40) {
+                    type = 3;
+                }
+                else if (RHin <= 50) {
+                    type = 2;
+                }
+                else if (RHin <= 60) {
+                    type = 1;
+                }
+
+                console.log(type);
+
+                io.emit("number", type);
 
                 request('http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&units=metric&APPID=e450050cc136b1b2d3598501a1489045', function (error, response, body) {
                     if (!error && response.statusCode == 200) {
 
                         var weather = JSON.parse(body); // parse weather data
-                        var To = weather.main.temp; // outdoor air temperature, deg C
-                        var RHo = weather.main.humidity; // relative humidity of outdoor air, %
 
-                        var S = 200; // emission rate of water vapor into indoor air from various sources, gm/hour
-                        var A = 158; // floor area of house, m^2
-                        var Hc = 2.8; // average ceiling height, m
-                        var V = A * Hc; // house volume, m^3
-                        var AER = 0.25; // air exchange rate, exchanges/hr
-                        var Alpha = 0.65; // rate of water vapor uptake into building materials, fraction/hr
-                        var Beta = 0.17; // rate of water vapor release from building materials, fraction/hr
-                        var Tin = 20; // indoor temperature, deg C
-                        var Wsat = (100000 * (6.112 * Math.exp(17.62 * Tin / (243.12 + Tin))) / ((273.15 + Tin) * 461.5)); // saturated water vapor concentration at indoor temperature, gm/m^3
-                        var Wo = 216.7 * (RHo / 100) * (6.112 * Math.exp((17.62 * To) / (243.12 + To))) / (273.15 + To); //Calculates absolute humidity of outdoor air, gm/m^3
-                        var Win = (S / V + AER * Wo + Beta * Wsat) / (AER + Alpha); // calculates water vapor concentration in indoor air, gm/m^3
-                        var RHin = (Win / Wsat) * 100; // relative humidity of indoor air, %
 
-                        var type = 0;
-                        if (RHin <= 30) {
-                            type = 4;
-                        }
-                        else if (RHin <= 40) {
-                            type = 3;
-                        }
-                        else if (RHin <= 50) {
-                            type = 2;
-                        }
-                        else if (RHin <= 60) {
-                            type = 1;
-                        }
 
-                        console.log(RHin);
-                        io.emit("number", type);
                     }
+
+
+
                 });
 
-
             }
+
         });
 
 
